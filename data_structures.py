4d29efee77e7668ee81c585e0c6a9e7c75000db5
RAYYANHASSAN321 <rayyanhassan1688@gmail.com>
2025-12-26 10:11:01 +0500

    data_structures

--- data_structures.py
+++ data_structures.py
@@ -23,3 +23,32 @@ print(student)
 
 student.reverse()
 print(student)
+
+marks = [45 , 78 , 95 , 67 , 85 , 83 , 46]
+marks.sort()
+
+print(marks)
+
+# -------------------- Tuple --------------------
+# immutable and can have duplicate values
+
+employee = ("Ahmed" , "Lahore" , 45000 , "Manager" )
+
+print(employee)
+print(type(employee))
+print(employee.index(45000))
+
+print(employee[0])
+print(employee[-1])  #last element
+print(employee[1:3]) # slicing
+
+# count shows how many times the data appeared in tuple
+print(employee.count(45000))
+
+# unpacking of data in tuple data structure
+
+name , address , salary , designation  = employee
+print(name)
+print(address)
+print(designation)
+print(salary)
