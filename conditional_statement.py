c5e9adf6da6184bf0fa3eed939840ac3a29a78df
RAYYANHASSAN321 <rayyanhassan1688@gmail.com>
2025-12-31 09:51:35 +0500

    new

--- conditional_statement.py
+++ conditional_statement.py
@@ -25,3 +25,32 @@ elif (percentage >= 50):
 else:
     print("Fail")
     
+#-------------------Nested if-----------------------#
+
+num = int(input("Enter number : "))
+
+if(num > 0):
+    
+    if(num % 2 == 0):
+        print(f"{num} is even number")
+        
+    else:
+        print(f"{num} is odd number")
+
+else:
+    print(f"number should be greater than 0")
+
+
+#-------------------Ternary operator -------------------#
+
+age = int(input("Enter age : "))
+
+status = "Adult" if age >= 18 else "Teenage"
+
+print(status)
+
+percent = int(input("Enter percentage : "))
+
+grade = "A+" if percent >= 80 else "A" if percent >= 70 else "B" if percent >= 60 else "Fail"
+
+print(grade)
